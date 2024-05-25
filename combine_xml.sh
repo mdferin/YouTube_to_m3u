ff71cf196419ef3ad3255532915a3a8dd427722b
mdferin <85060524+mdferin@users.noreply.github.com>
2024-05-25 09:39:22 +0800

    Update combine_xml.sh

--- combine_xml.sh
+++ combine_xml.sh
@@ -4,5 +4,9 @@
 wget -O unifi_epg.xml https://raw.githubusercontent.com/azimabid00/epg/main/unifi_epg.xml
 wget -O astro_epg.xml https://raw.githubusercontent.com/azimabid00/epg/main/astro_epg.xml
 
+# Remove specified text from astro_epg.xml
+sed -i '/<tv generated-by="azimabid00 on github - EPG XML Generator V1.0 - EPG Generated for 25-05-2024">/d' astro_epg.xml
+sed -i '/<?xml version="1.0" encoding="UTF-8"?>/d' astro_epg.xml
+
 # Combine XML files
 cat unifi_epg.xml astro_epg.xml > Rancak.xml
